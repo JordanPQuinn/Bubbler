e07c563c0fb08e3edcb265e0bd5ea500519a4b18
Amy Russell <russell.amyh@gmail.com>
2017-12-06 10:18:32 -0700

    Starting bubble tests

--- test/bubble-test.js
+++ test/bubble-test.js
@@ -1,3 +1,4 @@
+global.Image = class {}
 const chai = require('chai');
 const Bubble = require('../lib/Bubble.js');
 const assert = chai.assert;
@@ -8,5 +9,33 @@ describe('Bubble', function() {
     assert.equal('true', 'true')
   });
 
+  it('should be a constructor', function() {
+    assert.isFunction(Bubble)
+  })
   
+  it('should take arguments', function() {
+    newBubble = new Bubble(10, 10, 30, 30)
+    assert.equal(newBubble.x, 10)
+  })
+
+  it('should respawn if there are lives left', function() {
+    newBubble = new Bubble(10, 10, 30, 30)
+    newBubble.lives = 3;
+    newBubble.respawn();
+    assert.equal(newBubble.alive, true)
+  })
+
+  it('should move up if the up arrow is pressed', function() {
+    newBubble = new Bubble(10, 10, 30, 30)
+    newBubble.y = 100;
+    let event = {};
+    event.key = 'ArrowUp'
+    newBubble.move(event);
+    assert.equal(newBubble.y, 65)
+  })
+
+  it('should float in the top half of the game', function() {
+    newBubble = new Bubble(10, 10, 30, 30)
+    
+  })
 })
